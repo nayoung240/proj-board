74a7fb58d9563a760bd93e1c3bf6acc326b3bad6
zerome <iirony@naver.com>
2022-07-24 17:54:29 +0900

    #9 - 도메인 클래스 설계 (게시글, 댓글)

---
+++ fc-project-board/src/main/java/com/fc/projectboard/domain/ArticleComment.java
@@ -0,0 +1,13 @@
+package com.fc.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private String content;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
