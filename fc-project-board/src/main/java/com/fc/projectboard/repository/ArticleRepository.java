ef836b775340b30eab08b95ac1e3f3802dbe4b31
zerome <iirony@naver.com>
2022-08-01 01:28:29 +0900

    #11 - repository interface 작성

---
+++ fc-project-board/src/main/java/com/fc/projectboard/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.fc.projectboard.repository;
+
+import com.fc.projectboard.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
