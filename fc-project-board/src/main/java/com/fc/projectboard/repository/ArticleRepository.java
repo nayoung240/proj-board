7bff96f109bbe412dc1eb298ecd462b31e815c99
zerome <iirony@naver.com>
2022-08-07 19:02:16 +0900

    #6 - data rest 설정

--- fc-project-board/src/main/java/com/fc/projectboard/repository/ArticleRepository.java
+++ fc-project-board/src/main/java/com/fc/projectboard/repository/ArticleRepository.java
@@ -2,8 +2,10 @@ package com.fc.projectboard.repository;
 
 import com.fc.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
