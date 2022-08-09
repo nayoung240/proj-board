749fabaeb2fb271695f2979ca5dae6ec53701d68
zerome <iirony@naver.com>
2022-08-10 00:28:37 +0900

    #17 - Querydsl  지원을 이용하여 api 검색 옵션 추가

--- fc-project-board/src/main/java/com/fc/projectboard/repository/ArticleCommentRepository.java
+++ fc-project-board/src/main/java/com/fc/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,27 @@
 package com.fc.projectboard.repository;
 
-import com.fc.projectboard.domain.Article;
+import com.fc.projectboard.domain.ArticleComment;
+import com.fc.projectboard.domain.QArticle;
+import com.fc.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<Article, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+        @Override
+        default void customize(QuerydslBindings bindings, QArticleComment root) {
+            bindings.excludeUnlistedProperties(true); // 리스팅에 포함시키지 않은 property는 검색에서 제외
+            bindings.including(root.content, root.createdAt, root.createdBy);
+            bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%s{v}%' 인덱스를 타지 못할 수 있다
+            bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+            bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // like '%s{v}%' 인덱스를 타지 못할 수 있다
+        }
 }
