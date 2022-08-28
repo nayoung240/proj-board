2dbea01bbe20100330292438585c82885170790a
zerome <iirony@naver.com>
2022-08-28 22:15:56 +0900

    #21 - 게시글 기능 테스트

---
+++ fc-project-board/src/test/java/com/fc/projectboard/service/ArticleServiceTest.java
@@ -0,0 +1,96 @@
+package com.fc.projectboard.service;
+
+import com.fc.projectboard.domain.Article;
+import com.fc.projectboard.domain.type.SearchType;
+import com.fc.projectboard.dto.ArticleDto;
+import com.fc.projectboard.dto.ArticleUpdateDto;
+import com.fc.projectboard.repository.ArticleRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.BDDAssumptions.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willDoNothing;
+
+@DisplayName("비즈니스 로직 - 게시글")
+@ExtendWith(MockitoExtension.class)
+class ArticleServiceTest {
+    @InjectMocks private ArticleService sut;
+    @Mock private ArticleRepository articleRepository;
+
+    // 검색, 페이지네이션, 정렬
+    @DisplayName("게시글을 검색하면 게시글 리스트를 반환한다")
+    @Test
+    void givenSearchParameters_whenSearchingArticles_thenReturnArticleList() {
+        // Given
+
+        // When
+        Page<ArticleDto> articles = sut.searchArticles(SearchType.TITLE, "search keyword");
+
+        // Then
+        assertThat(articles).isNotNull();
+    }
+
+    // 각 게시글 페이지로 이동
+    @DisplayName("게시글을 조회하면, 게시글 반환한다")
+    @Test
+    void givenArticleId_whenSearchingArticle_thenReturnArticle() {
+        // Given
+
+        // When
+        ArticleDto articles = sut.searchArticles(1L);
+
+        // Then
+        assertThat(articles).isNotNull();
+    }
+
+    @DisplayName("게시글 정보를 입력하면, 게시글을 생성한다")
+    @Test
+    void givenArticleInfo_whenSavingArticle_thenSaveArticle() {
+        // Given
+        given(articleRepository.save(any(Article.class))).willReturn(null);
+
+        // When
+        sut.saveArticle(ArticleDto.of(LocalDateTime.now(), "Uno", "title", "content", "#java"));
+
+        // Then
+        then(articleRepository).should().save(any(Article.class));
+    }
+
+    @DisplayName("게시글 ID와 수정 정보를 입력하면, 게시글을 수정한다")
+    @Test
+    void givenArticleIdAndModifiedInfo_whenUpdatingArticle_thenUpdateArticle() {
+        // Given
+        given(articleRepository.save(any(Article.class))).willReturn(null);
+
+        // When
+        sut.updateArticle(1L, ArticleUpdateDto.of("title", "content", "#java"));
+
+        // Then
+        then(articleRepository).should().save(any(Article.class));
+    }
+
+    @DisplayName("게시글 ID를 입력하면, 게시글을 삭제한다")
+    @Test
+    void givenArticleId_whenDeletingArticle_thenDeletesArticle() {
+        // Given
+        willDoNothing().given(articleRepository).delete(any(Article.class));
+
+        // When
+        sut.deleteArticle(1L);
+
+        // Then
+        then(articleRepository).should().delete(any(Article.class));
+    }
+
+}
